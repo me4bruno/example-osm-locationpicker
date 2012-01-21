1b78deb6a840b4b79e452268effdb47b46129a4a
me4bruno <a.bruns@gmx.de>
2012-01-21 09:37:31 +0100

    clean up

--- src/main/java/de/bruns/example/osm/locationpicker/PoiServlet.java
+++ src/main/java/de/bruns/example/osm/locationpicker/PoiServlet.java
@@ -2,7 +2,6 @@ package de.bruns.example.osm.locationpicker;
 
 import java.io.IOException;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
